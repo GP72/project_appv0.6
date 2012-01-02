249b8ddbb1b6149c91227ae5ec3601d4de531481
Graeme Phelan <greyphelan@gmail.com>
2012-01-02 00:03:59 +0000

    Made change to home named route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ProjectAppv06::Application.routes.draw do
 
-  root :to =>"pages#home"
+  root :to =>'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
