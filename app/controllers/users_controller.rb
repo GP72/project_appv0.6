afc81ac26a869c95ab33adff4ebc02470e834d3f
Graeme Phelan <greyphelan@gmail.com>
2012-01-05 01:07:12 +0000

    Added eidit functionality to site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,25 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the FACE SPACE!"
-      redirect_to @user
+      redirect_to @user, :flash[:success] = "Welcome to the FACE SPACE!"
     else
       @title = "Sign up"
       render 'new'
+    end
+  end
+ 
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+     redirect_to @user, :flash[:success] = "Profile updated."
+    else
+    @title = "Edit user"
+    render 'edit'
     end
   end
 end
