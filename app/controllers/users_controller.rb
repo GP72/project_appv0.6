76d5050d6d35a10e622dac1c4b17fac523599a55
Graeme Phelan <greyphelan@gmail.com>
2012-01-06 01:15:55 +0000

    Added admin rights to user 1, a destroy function and delete links to each user accessible only to the site administrator (user1)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_filter :authenticate, :only => [:index, :edit, :update]
-before_filter :correct_user, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
 
   def new
    @user = User.new
@@ -42,6 +43,12 @@ before_filter :correct_user, :only => [:edit, :update]
     end
   end
 
+ def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
 private
 
     def authenticate
@@ -51,5 +58,9 @@ private
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
